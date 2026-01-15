c37c1c159647a7aed48799665059b53b2a3494b4
Cersousagit <92043060+Cersousagit@users.noreply.github.com>
2026-01-15 16:00:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -84,13 +84,15 @@ def marcar_presenca(index):
     return redirect("/admin")
 
 
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)  
-
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/")
+
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
