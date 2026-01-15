776dd1a02cf6da057314f4a4e5ddf5befff2c8b5
Cersousagit <92043060+Cersousagit@users.noreply.github.com>
2026-01-15 15:42:34 -0300

    Update app.py
    adicionando rota de presença

--- app.py
+++ app.py
@@ -71,6 +71,17 @@ def admin():
 
     return render_template("admin.html", agendamentos=ativos)
 
+@app.route("/presente/<int:index>")
+def marcar_presenca(index):
+    if session.get("perfil") != "admin":
+        return redirect("/")
+
+    global agendamentos
+
+    if index < len(agendamentos):
+        agendamentos[index]["presente"] = True
+
+    return redirect("/admin")
 
 
 import os
